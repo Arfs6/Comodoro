38c8da5456b89597309fb328883d033777642e62
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-05-25 18:27:55 +0100

    fixed libvlc not included in cxfreeze build

--- src/engine/__init__.py
+++ src/engine/__init__.py
@@ -5,6 +5,7 @@ app communicates with. communication is done via socket (check messenger.py).
 """
 
 import sys
+import os
 from logging import getLogger
 
 import logger
@@ -15,8 +16,11 @@ from .controller import Controller
 log = getLogger(__name__)
 
 
-def run():
+def run() -> None:
+    """Perform pre-startup tasks and start controller"""
+    log.debug("starting engine...")
     sys.excepthook = logUnhandledExceptions
+    setVlcLib()
     Controller()
 
 
@@ -25,3 +29,15 @@ def logUnhandledExceptions(excType: type, excValue: Exception, traceback)\
     """Writes the unhandled exception as an Warning to the log file"""
     log.warning("Uncaught exception", exc_info=(excType, excValue, traceback),
                 stack_info=True, stacklevel=2)
+
+
+def setVlcLib() -> None:
+    """Checks if app is frozen and set libvlc library"""
+    if not getattr(sys, 'frozen', False):
+        # running from source
+        return
+
+    if sys.platform == 'win32' and os.path.exists('libvlc.dll'):
+        os.environ['PYTHON_VLC_LIB_PATH'] = os.path.join(os.getcwd(),
+                                                         'libvlc.dll')
+    # todo: do for other platforms
--- src/setup.py
+++ src/setup.py
@@ -1,30 +1,62 @@
 # -*- coding: utf-8 -*-
 """Let's build an executable for Comodoro"""
 
-from cx_Freeze import setup, Executable
 import sys
+import os
+import vlc
+from cx_Freeze import setup, Executable
+from typing import Union, List
 
 from appinfo import appInfo
 
 
-base = "Win32GUI" if sys.platform == "win32" else None
-executables = [
-        Executable("main.pyw", base=base, target_name=appInfo.appName),
-        Executable("runEngine.py", target_name='engine')
-        ]
-options = {
-        "build_exe": {
-            "packages": [],
-            "includes": [],
-            "excludes": ['tkinter', 'multiprocessing', 'ensurepip', 'test'],
-            "include_files": ['config_spec.ini', 'sounds'],
+def run() -> None:
+    """Start setup process"""
+    base = "Win32GUI" if sys.platform == "win32" else None
+    executables = [
+            Executable("main.pyw", base=base, target_name=appInfo.appName),
+            Executable("runEngine.py", target_name='engine')
+            ]
+    options = {
+            "build_exe": {
+                "packages": [],
+                "includes": [],
+                "excludes": ['tkinter', 'multiprocessing', 'ensurepip', 'test'],
+                "include_files": ['config_spec.ini', 'sounds'] +
+                getVlcLib(),
+                }
             }
-        }
 
-setup(
-        name=appInfo.appName,
-        description=appInfo.description,
-        version=appInfo.version,
-        options=options,
-        executables=executables
-        )
+    setup(
+            name=appInfo.appName,
+            description=appInfo.description,
+            version=appInfo.version,
+            options=options,
+            executables=executables
+            )
+
+
+def getVlcLib() -> List[str]:
+    """Get location of vlc library"""
+    path: Union[str, None] = vlc.find_lib()[1]
+    if not path:
+        return []
+
+    paths: List[str] = [os.path.join(path, 'libvlc.dll')]
+    # include plugin folder
+    # remove gui related plugins
+    path = os.path.join(path, 'plugins')
+    ls: List[str] = os.listdir(path)
+    excludes: List[str] = ['control', 'gui', 'lua', 'plugins.dat', 'visualization']
+    for file in ls:
+        if file in excludes:
+            continue
+        source = os.path.join(path, file)
+        dest = os.path.join('plugins', file)
+        paths.append((source, dest))
+
+    return paths
+
+
+if __name__ == '__main__':
+    run()
